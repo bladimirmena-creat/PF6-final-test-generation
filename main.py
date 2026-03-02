b3ecd08d6aa99e314ee396d6d9954e7c6a528b43
bladimirmena-creat <bladimir.mena@genstudents.org>
2026-03-02 13:40:02 -0500

    codigo final propiol

--- main.py
+++ main.py
@@ -1,34 +1,17 @@
 import requests
 import json
 
-response = requests.get("https://api-colombia.com/api/v1/TypicalDish")
-platos = json.loads(response.content)
-
 def dish_fetch(num):
-   
-    if 0 < num <= len(platos):
-        p = platos[num - 1]  
-        return {
-            "id": num,  
-            "name": p.get("name", "Desconocido"),
-            "description": p.get("description", "Sin descripción"),
-            "ingredients": p.get("ingredients", "Sin ingredientes")
-        }
-    else:
-        
-        return {"id": num, "name": "Plato no encontrado", "description": "", "ingredients": ""}
-
+    # Conexión directa a la API usando el número ingresado
+    url = requests.get (f"https://api-colombia.com/api/v1/TypicalDish/{num}")
+    
+    plato = json.loads(url.content)
+    return plato
 
 def main():
-    print("Platos disponibles:")
-    for i, p in enumerate(platos, start=1):
-        print(f"{i}. {p.get('name', 'Desconocido')}")
-    
-    try:
-        n = int(input("Ingrese número del plato: "))
-        print(dish_fetch(n)) 
-    except:
-        print({"error": "Entrada inválida"})
+    n = int(input("Ingrese el número del plato: "))
+    resultado = dish_fetch(n)
+    print(resultado)
 
 if __name__ == "__main__":
     main()
